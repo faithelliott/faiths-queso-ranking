137638a6281b88c33054db78418635027206d244
CBernard23 <clbernard2016@icloud.com>
2019-10-24 19:39:45 -0500

    rewrote queso cards to map array

--- src/components/queso.js
+++ src/components/queso.js
@@ -6,89 +6,36 @@ import Draggable, {DraggableCore} from 'react-draggable';
 
 console.log("render");
 
-
+    const choices = [
+      { name: 'Mexican Village - standard queso', position: 1 },
+      { name: 'Apple bees - taco topped queso', position: 2 },
+      { name: 'Mexican Village - Queso with taco meat', position: 3 },
+      { name: 'Paradisios - queso trio', position: 4 },
+      { name: 'Buffalo Wild Wings - Queso', position: 5 },
+      { name: 'Chiles - Queso', position: 6 },
+    ]
 
     const queso = ({ quesos }) => {
       return (
-     
-         <div className="divSize">
-          <p>
-          <Draggable>
-            <div>
-              <Card>
-                  #1.Mexican Village - standard queso
-              </Card>
-                <div class="drip-1"></div>
-                <div class="drip-2"></div>
-                <div class="drip-3"></div>
-           </div>
-           </Draggable>
-          </p>
-          <p>
-          <Draggable>
-            <div className="divSize">
-              <Card>
-                  #2.Apple bees - taco topped queso
-              </Card>
-                <div class="drip-1"></div>
-                <div class="drip-2"></div>
-                <div class="drip-3"></div>
-                </div>
-           </Draggable>
-          </p> 
-          <p>
-          <Draggable>
-            <div className="divSize">
-              <Card>
-                  #3.Mexican Village - Queso with taco meat
-              </Card>
-                <div class="drip-1"></div>
-                <div class="drip-2"></div>
-                <div class="drip-3"></div>
-                </div>
-           </Draggable>
-          </p> 
-          <p>
-          <Draggable>
-            <div className="divSize">
-              <Card>
-                  #4.Paradisios - queso trio
- 
-              </Card>
-                <div class="drip-1"></div>
-                <div class="drip-2"></div>
-                <div class="drip-3"></div>
-                </div>
-           </Draggable>
-          </p> 
-          <p>
-          <Draggable>
-            <div className="divSize">
-              <Card>
-                  #5.Buffalo Wild Wings - Queso 
-              </Card>
-                <div class="drip-1"></div>
-                <div class="drip-2"></div>
-                <div class="drip-3"></div>
-                </div>
-           </Draggable>
-          </p> 
-          <p>
-          <Draggable>
-            <div className="divSize">
-              <Card>
-                  #6.Chiles - Queso 
-                
-              </Card>
-                <div class="drip-1"></div>
-                <div class="drip-2"></div>
-                <div class="drip-3"></div>
-                </div>
-           </Draggable>
-          </p> 
+        <div>
+          {choices.map(choice =>
+            <p>
+            <Draggable
+            axis="y"
+            grid={[125, 125]}>
+              <div className="divSize">
+                <Card>
+                  #{choice.position}. {choice.name}
+                </Card>
+                  <div class="drip-1"></div>
+                  <div class="drip-2"></div>
+                  <div class="drip-3"></div>
+             </div>
+             </Draggable>
+            </p>
+          )}
         </div>
-       
       )
     };
-   
+
     export default queso;
